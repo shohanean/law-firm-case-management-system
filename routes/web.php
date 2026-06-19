0eee8b9c5e7715ea33bc862427b173d10178d7e0
shohanean <hossain_shohan1@live.com>
2026-06-19 17:34:19 +0600

    issue sorted

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\CaseController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProjecttypeController;
+use App\Http\Controllers\ProjectTypeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StatusController;
 use App\Http\Controllers\UserController;
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
-    Route::resource('projecttype', ProjectTypeController::class);
+    Route::resource('project-type', ProjectTypeController::class)->names('project_type');
     Route::resource('case-status', StatusController::class)->names('status');
     Route::resource('user', UserController::class)->except(['show', 'create']);
     Route::resource('case', CaseController::class);
